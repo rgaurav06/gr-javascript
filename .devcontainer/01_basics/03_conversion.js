2fefe4149fa7c1781d4997f32f6c5009a01a7cad
rgaurav06 <rgaurav23extc@student.mes.ac.in>
2026-03-14 10:18:42 +0000

    new

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,4 @@
+let hii="3"
+let nohii= Number(hii)
+console.log(nohii);
+console.log(typeof nohii);
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,7 @@
 console.log("Gaurav")
+const name="oh"
+let accid="93"
+var city="dub"
+
+accid="39"
+console.table([accid,city])
