64d77c380d9f46d34ce0970206fbd39c1cfc170d
rgaurav06 <rgaurav23extc@student.mes.ac.in>
2026-03-16 18:04:11 +0000

    remove cunwanted

---
+++ .devcontainer/01_basics/01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+/*console.log("2"===2);
+console.log("2"==2);
+console.log(null>=0);
+
+let user
+console.log(typeof user)*/
+
+// heap and stack memory
+
+let play1="judo";
+play2=play1;
+console.log(play1);
+play2="boxing";
+console.log(play2);
+
+
+
+const objectName = {
+ email: "hello",
+email2: "world",
+ // ... more key-value pairs
+};
+const objectName2 = objectName
+objectName.email="hi";
+console.log(objectName);
+console.log(objectName2);
---
+++ .devcontainer/01_basics/01_basics/05_string.js
@@ -0,0 +1,6 @@
+//const name = "gaurav";
+//console.log(`hello ${name}`);
+const game=new String("cricket");
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(4));
