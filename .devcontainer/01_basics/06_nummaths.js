f36398c041d370ac36318caf4e6c728e0620987d
rgaurav06 <rgaurav23extc@student.mes.ac.in>
2026-03-16 19:04:38 +0000

    new

---
+++ .devcontainer/01_basics/06_nummaths.js
@@ -0,0 +1,12 @@
+// const age= 30;
+// console.log(age);
+// const num=100000000000000
+// console.log(num.toLocaleString('en-IN'));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
