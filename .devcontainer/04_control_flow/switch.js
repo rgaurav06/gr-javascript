fe2d1af9b3505472d526514332cf44f8cb98a47c
rgaurav06 <rgaurav23extc@student.mes.ac.in>
2026-03-23 19:45:38 +0000

    new

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,18 @@
+const month= 3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+
+    default:
+        break;
+}
