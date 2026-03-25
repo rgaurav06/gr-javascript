8ab0b984f00a794ac56270abecd31ce23dff8fd3
rgaurav06 <rgaurav23extc@student.mes.ac.in>
2026-03-25 19:39:40 +0000

    new

---
+++ .devcontainer/05_iterations/three.js
@@ -0,0 +1,47 @@
+const result=[32,34 , 39,36]
+for (const marks of result) {
+    console.log(marks);
+    
+}
+
+
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
