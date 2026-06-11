346876ee24eab076b0be1e110d6e6a5d73d0603f
rgaurav06 <rgaurav23extc@student.mes.ac.in>
2026-06-11 18:06:33 +0000

    new

--- .devcontainer/01_basics/07_dates.js
+++ .devcontainer/01_basics/07_dates.js
@@ -1,13 +1,13 @@
-// // let mydate= new Date();
-// // console.log(mydate);
-// // console.log(mydate.toString());
-// // console.log(mydate.toDateString());
-// // console.log(mydate.toLocaleDateString());
-// let mdate= new Date(2020, 11, 25);
-// console.log(mdate.toDateString());
-// console.log(mdate.getFullYear());
-// console.log(mdate.getMonth());
-// console.log(mdate.getDate());
+// let mydate= new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+let mdate= new Date(2020, 0, 25);
+console.log(mdate.toDateString());
+console.log(mdate.getFullYear());
+console.log(mdate.getMonth()+1);
+console.log(mdate.getDate());
 let mytime = Date.now()
 let mydate = Date();
 console.log(mydate);
